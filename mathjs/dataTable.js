7488a181735968478a4ff1f057ec14c437a9f922
steve <za19012397@zapopan.tecmm.edu.mx>
2025-03-17 21:21:27 -0600

    feat: Se creo un archivo para la estructura de los datos de un archivo de csv

---
+++ mathjs/dataTable.js
@@ -0,0 +1,27 @@
+import file from 'fs'
+
+export default class DataTable{
+
+    headers=null
+    information = null
+
+    setHeaders(listHeaders){
+        this.headers = listHeaders
+    }
+
+    readCsv(ruta){
+        return new Promise((resolve,reject)=>{
+            file.readFile(ruta,'utf8',(err,data)=>{
+                if(err){
+                    console.log('Error reading the file:',err)
+                    return
+                }
+                const rows = data.split('\n')
+                resolve(rows)
+            })
+        })
+
+    }
+
+
+}
