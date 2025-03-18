9329e318282cca844368a738f4612bb0efa1bfc3
steve <za19012397@zapopan.tecmm.edu.mx>
2025-03-17 21:22:28 -0600

    feat: Se agrego una prueba para la clase de data table

---
+++ index.js
@@ -0,0 +1,12 @@
+import DataTable from "./mathjs/dataTable.js"
+
+const tabla = new DataTable()
+
+tabla.readCsv('./customers-100.csv')
+    .then((rows)=>{
+        //console.log(rows)
+        console.log(rows[0])
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
