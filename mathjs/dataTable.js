40451fa6fd1048c8cc921c59f41c318f54df0675
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2025-03-20 16:53:06 -0600

    feat: Se agrego una funcion para procesar la informacion del archivo csv, se espera que se pueda crear un objeto para el arreglo de informacion

--- mathjs/dataTable.js
+++ mathjs/dataTable.js
@@ -25,15 +25,32 @@ export default class DataTable{
 
     buildTable(ruta){
         return new Promise((resolve,reject)=>{
+
             file.readFile(ruta,'utf8',(err,data)=>{
                 if(err){
                     console.log('Error reading the file:'.err)
                 }
-                const rows = data.split('\n')
+                let rows = data.split('\n')
                 this.headers = rows[0].split(',')
+
+                roww.shift()
+
+                rows.map(function(element){
+                  let values = element.split(',')
+
+                  function  buildObject(iterador,index,array){
+                    this.information.push({iterador:values[index]})
+                    return iterdador
+                  }
+
+                  this.headers.forEach(buildObject)
+                })
+                
+                resolve
             })
         })
     }
+
 
 
 }
